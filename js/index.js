0314f46da3f9ecf6145cdc4a1d992d3dcbb779c0
Erick Peña <nero2204.gs@gmail.com>
2020-10-25 14:38:59 -0600

    version 0.9.6

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const   target = document.getElementById('menu-rapido'),
+        btnArriba = document.getElementById('icono-arriba'),
+        menuRapido = document.getElementById('menu-rapido'),
+        hold = 1;
+
+function go(id){
+    let element = document.getElementById(id);
+    element.scrollIntoView({block: "start", behavior: "smooth"});
+}
+
+const observer = new IntersectionObserver(interseccion, {
+    threshold: hold
+});
+observer.observe(target);
+
+function interseccion(events){
+    let event = events[0];
+    let visible = event.intersectionRatio >= hold;
+    if(visible){
+        btnArriba.removeEventListener('click', irArriba);
+        btnArriba.classList.add('oculto');
+    }else{
+        btnArriba.addEventListener('click', irArriba);
+        btnArriba.classList.remove('oculto');
+    }
+}
+
+function irArriba(){
+    menuRapido.scrollIntoView({block: "end", behavior: "smooth"});
+}
+
+
